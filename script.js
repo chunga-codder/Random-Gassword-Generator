eed36e698b75a7d2597eb02d3893dd10d63517e8
chunga <clintonchunga@gmail.com>
2020-10-09 07:33:05 +0100

    trying to make my slider bar responsive with respect to my password length

--- script.js
+++ script.js
@@ -1,58 +1,59 @@
+  
 var lowercase = "abcdefghijklmnopqrstuvwxyz",// sasigning my characters to var 
-  uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
-  numbers = "0123456789",
-  punctuation = "!@#$%^&*()_+~`|}{[]:;?><,./-=",
-  
-  lowercaseInput = document.getElementById("lowercase"),//setting a short hand for a cleaner code
-  uppercaseInput = document.getElementById("uppercase"),
-  punctuationInput = document.getElementById("punctuation"),
-  numbersInput = document.getElementById("numbers"),
-  lengthInput = document.getElementById("length"),
-  passwordFeild = document.getElementById("password"),
-  generateButton = document.getElementById("generate"),
-  copyButton = document.getElementById("copy"),
-  plength,
-  userPassword,
-  passwordCharSet;
+uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+numbers = "0123456789",
+punctuation = "!@#$%^&*()_+~`|}{[]:;?><,./-=",
 
-  //setting my generate function to be called on an onclick on generate
- 
-function generate() {
-  userPassword = "";
-  passwordCharSet = "";
+lowercaseInput = document.getElementById("lowercase"),//setting a short hand for a cleaner code
+uppercaseInput = document.getElementById("uppercase"),
+punctuationInput = document.getElementById("punctuation"),
+numbersInput = document.getElementById("numbers"),
+lengthInput = document.getElementById("length"),
+passwordFeild = document.getElementById("password"),
+generateButton = document.getElementById("generate"),
+copyButton = document.getElementById("copy"),
+plength,
+userPassword,
+passwordCharSet;
 
-  //setting my if conditions for my check boxes,which will add the password charset to my cheked boxgi
-  if (lowercaseInput.checked) {
-    passwordCharSet += lowercase;
-  }
-  if (uppercaseInput.checked) {
-    passwordCharSet += uppercase;
-  }
-  if (punctuationInput.checked) {
-    passwordCharSet += punctuation;
-  }
-  if (numbersInput.checked) {
-    passwordCharSet += numbers;
-  }
-  plength = Number(lengthInput.value);
- 
-  for (let i = 0; i < plength; i++) {
-    userPassword += passwordCharSet.charAt(
-      Math.floor(Math.random() * passwordCharSet.length)
-    );
-   // setting an alert for users to make sure they chose a check box
-  }
-  if (userPassword == "") {
-    let alertbox = document.getElementById('alert');
-    alertbox.innerHTML = "Please select atleast one option"
-    alertbox.classList.add('fail');
-    setTimeout(function(){ 
-      alertbox.classList.remove('fail');
-    }, 3000);
-  } else {
-    passwordFeild.innerHTML = userPassword;
-  }
-  copyButton.setAttribute("data-clipboard-text", userPassword)
+//setting my generate function to be called on an onclick on generate
+function outputUpdate(){
+    document.querySelector('#slider').value =length
 }
-  
+function generate() {
+userPassword = "";
+passwordCharSet = "";
+
+//setting my if conditions for my check boxes,which will add the password charset to my cheked boxgi
+if (lowercaseInput.checked) {
+  passwordCharSet += lowercase;
+}
+if (uppercaseInput.checked) {
+  passwordCharSet += uppercase;
+}
+if (punctuationInput.checked) {
+  passwordCharSet += punctuation;
+}
+if (numbersInput.checked) {
+  passwordCharSet += numbers;
+}
+plength = Number(lengthInput.value);
+
+for (let i = 0; i < plength; i++) {
+  userPassword += passwordCharSet.charAt(
+    Math.floor(Math.random() * passwordCharSet.length)
+  );
+ // setting an alert for users to make sure they chose a check box
+}
+if (userPassword == "") {
+  let alertbox = document.getElementById('alert');
+  alertbox.innerHTML = "Please select atleast one option"
+  alertbox.classList.add('fail');
+  setTimeout(function(){ 
+    alertbox.classList.remove('fail');
+  }, 3000);
+} else {
+  passwordFeild.innerHTML = userPassword;
+}
+}
 generateButton.addEventListener("click", generate);
