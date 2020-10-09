9047a022a916664311987584dd6920fda270b129
chunga <clintonchunga@gmail.com>
2020-10-09 07:32:32 +0100

    restructuring my code so everything feats contend

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Assignment Code
-var clipboard = new Clipboard('.copy')
+
 var lowercase = "abcdefghijklmnopqrstuvwxyz",
   uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
   numbers = "0123456789",
@@ -40,7 +40,7 @@ function generate() {
   }
   if (userPassword == "") {
     let alertbox = document.getElementById('alert');
-    alertbox.innerHTML = "Please select an option before generating"
+    alertbox.innerHTML = "Please select atleast one option"
     alertbox.classList.add('fail');
     setTimeout(function(){ 
       alertbox.classList.remove('fail');
@@ -50,6 +50,7 @@ function generate() {
   }
   copyButton.setAttribute("data-clipboard-text", userPassword)
 }
+  
 generateButton.addEventListener("click", generate);
  
 clipboard.on('success', function(e) {
@@ -57,22 +58,10 @@ clipboard.on('success', function(e) {
     console.info('Text:', e.text);
     console.info('Trigger:', e.trigger);
     let alertbox = document.getElementById('alert');
-    alertbox.innerHTML = "Copied!"
     alertbox.classList.add('success');
     setTimeout(function(){ 
       alertbox.classList.remove('success');
     }, 3000);
     
     e.clearSelection();
-});
- 
-clipboard.on('error', function(e) {
-    console.error('Action:', e.action);
-    console.error('Trigger:', e.trigger);
-  let alertbox = document.getElementById('alert');
-    alertbox.innerHTML = "Try select the text to copy"
-    alertbox.classList.add('fail');
-    setTimeout(function(){ 
-      alertbox.classList.remove('fail');
-    }, 3000);
 });
