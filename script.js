363e4143b6be6c469bf1df5b9ef5214e536c2154
chunga <clintonchunga@gmail.com>
2020-10-09 07:34:56 +0100

    making some changes with my program like adjusting my java sript code

--- script.js
+++ script.js
@@ -1,14 +1,78 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
-
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
+var clipboard = new Clipboard('.copy')
+var lowercase = "abcdefghijklmnopqrstuvwxyz",
+  uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+  numbers = "0123456789",
+  punctuation = "!@#$%^&*()_+~`|}{[]:;?><,./-=",
+  lowercaseInput = document.getElementById("lowercase"),
+  uppercaseInput = document.getElementById("uppercase"),
+  punctuationInput = document.getElementById("punctuation"),
+  numbersInput = document.getElementById("numbers"),
+  lengthInput = document.getElementById("length"),
+  passwordFeild = document.getElementById("password"),
+  generateButton = document.getElementById("generate"),
+  copyButton = document.getElementById("copy"),
+  plength,
+  userPassword,
+  passwordCharSet;
+ 
+function generate() {
+  userPassword = "";
+  passwordCharSet = "";
+  if (lowercaseInput.checked) {
+    passwordCharSet += lowercase;
+  }
+  if (uppercaseInput.checked) {
+    passwordCharSet += uppercase;
+  }
+  if (punctuationInput.checked) {
+    passwordCharSet += punctuation;
+  }
+  if (numbersInput.checked) {
+    passwordCharSet += numbers;
+  }
+  plength = Number(lengthInput.value);
+ 
+  for (let i = 0; i < plength; i++) {
+    userPassword += passwordCharSet.charAt(
+      Math.floor(Math.random() * passwordCharSet.length)
+    );
+  }
+  if (userPassword == "") {
+    let alertbox = document.getElementById('alert');
+    alertbox.innerHTML = "Please select an option before generating"
+    alertbox.classList.add('fail');
+    setTimeout(function(){ 
+      alertbox.classList.remove('fail');
+    }, 3000);
+  } else {
+    passwordFeild.innerHTML = userPassword;
+  }
+  copyButton.setAttribute("data-clipboard-text", userPassword)
 }
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateButton.addEventListener("click", generate);
+ 
+clipboard.on('success', function(e) {
+    console.info('Action:', e.action);
+    console.info('Text:', e.text);
+    console.info('Trigger:', e.trigger);
+    let alertbox = document.getElementById('alert');
+    alertbox.innerHTML = "Copied!"
+    alertbox.classList.add('success');
+    setTimeout(function(){ 
+      alertbox.classList.remove('success');
+    }, 3000);
+    
+    e.clearSelection();
+});
+ 
+clipboard.on('error', function(e) {
+    console.error('Action:', e.action);
+    console.error('Trigger:', e.trigger);
+  let alertbox = document.getElementById('alert');
+    alertbox.innerHTML = "Try select the text to copy"
+    alertbox.classList.add('fail');
+    setTimeout(function(){ 
+      alertbox.classList.remove('fail');
+    }, 3000);
+});
